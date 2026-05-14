8d8a1f354c97633e37ee77daf9c9bf25a13ba106
jimj90201-a11y <jimj90201@gmail.com>
2026-05-14 17:01:39 +0530

    Update AdvanceRepairMod.java

--- src/main/java/com/example/advancerepair/AdvanceRepairMod.java
+++ src/main/java/com/example/advancerepair/AdvanceRepairMod.java
@@ -6,7 +6,8 @@ import net.minecraftforge.fml.common.Mod;
 @Mod("advancerepair")
 public class AdvanceRepairMod {
 
-    public AdvanceRepairMod() {
-        MinecraftForge.EVENT_BUS.register(AnvilHandler.class);
-    }
+public AdvanceRepairMod() {
+    MinecraftForge.EVENT_BUS.register(AnvilHandler.class);
+    MinecraftForge.EVENT_BUS.register(TooltipHandler.class);
+} 
 }
