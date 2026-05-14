41e8062639d64c0cf101a218d7ba65cb6381cbe9
jimj90201-a11y <jimj90201@gmail.com>
2026-05-14 16:57:21 +0530

    Update AnvilHandler.java

--- src/main/java/com/example/advancerepair/AnvilHandler.java
+++ src/main/java/com/example/advancerepair/AnvilHandler.java
@@ -3,7 +3,6 @@ package com.example.advancerepair;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
-import net.minecraft.core.component.DataComponents;
 
 import net.minecraftforge.event.AnvilUpdateEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -28,7 +27,7 @@ public class AnvilHandler {
             int newDamage = Math.max(0, left.getDamageValue() - repair);
             output.setDamageValue(newDamage);
 
-            boolean hasTrim = left.has(DataComponents.TRIM);
+            boolean hasTrim = left.getTag() != null && left.getTag().contains("Trim");
             boolean enchanted = left.isEnchanted();
 
             int xpCost;
